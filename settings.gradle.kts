97eeeeab0195f141970a17b4006f29a4fdda8bb2
Jauschua Stout <jauschua.stout@gmail.com>
2026-01-16 21:21:09 -0500

    fix(build): revert incorrect NewPipeExtractor submodule path

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -26,7 +26,7 @@ rootProject.name = "MyFlix"
 includeBuild("NewPipeExtractor") {
     dependencySubstitution {
         substitute(module("com.github.TeamNewPipe:NewPipeExtractor"))
-            .using(project(":NewPipeExtractor:extractor"))
+            .using(project(":extractor"))
     }
 }
 
